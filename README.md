f03c40327cab87f662c09765e2cd439ad0fa71c6
nhans32 <56236772+nhans32@users.noreply.github.com>
2021-11-09 20:17:34 -0800

    initial sklearn vectorization setup for comparison

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 Authors:
     Nicholas Hansen - nhanse02@calpoly.edu
-    Kaanan Kharwa - 
+    Kaanan Kharwa - *email here*
+
+Usage:
+    Text Vectorizer: python3 textVectorizer.py <dataset_path> <output_name>
+        Notes:
+            - <dataset_path> must be a directory containing the C50test and C50train directories
+            - <output_name> is path and filename of csv file to be created as a ground truth (omit .csv in this run parameter)
--- textVectorizer.py
+++ textVectorizer.py
@@ -1,5 +1,8 @@
 import sys
 import os
+import numpy as np
+import pandas as pd
+import sklearn.feature_extraction.text as sktext
 
 def parseVectorizerArgs(args):
     # if there are not 2 arguments then error
@@ -10,17 +13,32 @@ def parseVectorizerArgs(args):
     return args[0], args[1]
 
 def createGroundTruth(dataset_path, output_path):
+    documents = []
     with open(output_path + '.csv', 'w') as output_file:
         output_file.write('file_name,author\n')
         for dir in os.listdir(dataset_path):
             for author in os.listdir(dataset_path + '/' + dir):
                 for file in os.listdir(dataset_path + '/' + dir + '/' + author):
                     output_file.write(file + ',' + author + '\n')
+                    documents.append(dataset_path + '/' + dir + '/' + author + '/' + file)
         output_file.close()
+    return documents
 
 if __name__ == '__main__':
     dataset_path, output_path = parseVectorizerArgs(sys.argv[1:])
-    createGroundTruth(dataset_path, output_path)
+    documents = createGroundTruth(dataset_path, output_path)
 
 
-    print(dataset_path, output_path)
+    # --- SKLEARN VECTORIZERS (FOR COMPARISON) ---
+    sk_count_vectorizer = sktext.CountVectorizer(analyzer='word', input='filename')
+    sk_count_wm = sk_count_vectorizer.fit_transform(documents)
+    sk_count_tokens = sk_count_vectorizer.get_feature_names_out()
+    sk_wm_df = pd.DataFrame(sk_count_wm.toarray(), index=documents, columns=sk_count_tokens)
+
+    sk_tfidf_vectorizer = sktext.TfidfVectorizer(analyzer='word', input='filename')
+    sk_tfidf_wm = sk_tfidf_vectorizer.fit_transform(documents)
+    sk_tfidf_tokens = sk_tfidf_vectorizer.get_feature_names_out()
+    sk_tfidf_df = pd.DataFrame(sk_tfidf_wm.toarray(), index=documents, columns=sk_tfidf_tokens)
+    # ------
+
+    print(sk_tfidf_df)
